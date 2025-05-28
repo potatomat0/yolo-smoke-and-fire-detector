ce98285a38cf8b1a0d745e51b12bcee003284d93
Nhat Hoang Minh <56759973+potatomat0@users.noreply.github.com>
2025-05-28 15:16:51 +0700

    adjust custom save directory

--- localTest.py
+++ localTest.py
@@ -1,14 +1,17 @@
 from huggingface_hub import hf_hub_download
 from ultralytics import YOLO 
+import os 
 
 REPO_ID = "kappH/NhanDienKhoiLua" 
 FILENAME = "best.pt"  
-
+CURRENT_PATH = os.getcwd()
+PROJECT = './runs/detect'
+NAME = 'predict'
 model = YOLO(hf_hub_download(repo_id=REPO_ID, filename=FILENAME))
 
 # test ảnh bất kỳ 
 
-model.predict(source='example5.jpeg', imgsz=640, conf=0.25, save=True)
+model.predict(source='example5.jpeg', imgsz=640, conf=0.25, save=True, project=PROJECT, name=NAME)
 
 # test webcam
 
