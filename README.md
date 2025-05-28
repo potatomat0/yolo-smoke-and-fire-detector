1097033e65c2299e8f4ed1358bce73e72658d858
Nhat Hoang Minh <56759973+potatomat0@users.noreply.github.com>
2025-05-28 15:01:12 +0700

    second commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# How to run this:
+
+```bash
+git clone https://github.com/potatomat0/yolo-smoke-and-fire-detector.git
+
+cd yolo-smoke-fire-detector
+
+pip install requirement.txt
+
+python3 localTest.py
+```
--- localTest.py
+++ localTest.py
@@ -1,5 +1,4 @@
 from huggingface_hub import hf_hub_download
-import joblib
 from ultralytics import YOLO 
 
 REPO_ID = "kappH/NhanDienKhoiLua" 
@@ -9,9 +8,9 @@ model = YOLO(hf_hub_download(repo_id=REPO_ID, filename=FILENAME))
 
 # test ảnh bất kỳ 
 
-# model.predict(source='example5.jpeg', imgsz=640, conf=0.25, save=True)
+model.predict(source='example5.jpeg', imgsz=640, conf=0.25, save=True)
 
 # test webcam
 
-model.predict(source=0, imgsz=640, conf=0.25, show=True)
+# model.predict(source=0, imgsz=640, conf=0.25, show=True)
 
